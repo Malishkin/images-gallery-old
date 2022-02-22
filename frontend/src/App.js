bf3ee4eab0f93c1263bb6128b7820d6ccd4e59ad
Malishkin <malishkin2002@gmail.com>
2022-02-22 12:56:26 +0200

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,26 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
 
+const UNSPLASH_ACCESS_KEY = '';
+
 const App = () => {
   const [word, setWord] = useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(
+      `https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_ACCESS_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
   };
-  
+
+  console.log(process.env);
+ 
+
   return (
     <div>
       <Header title='Images Gallery' />
